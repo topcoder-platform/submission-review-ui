1118ea4910623957c8cee9d77b1907950b5ad9b0
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-03-13 01:00:12 +0200

    Fix CI

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const app = express()
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
-const port = process.env.PORT || 3000;
-app.listen(port);
+app.use(cors())
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
+const port = process.env.PORT || 3000
+app.listen(port)
 
 console.log(`App is listening on port ${port}`)
 
