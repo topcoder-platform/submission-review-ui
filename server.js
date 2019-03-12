027030a41d5e1b6b24e37aa85010010619bd8701
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-03-13 00:53:43 +0200

    Fix CI

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const app = express()
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, './build')))
-
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => res.sendFile(path.join(`${__dirname}/./build/index.html`)))
-
-const port = process.env.PORT || 3000
-app.listen(port)
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+const port = process.env.PORT || 3000;
+app.listen(port);
 
 console.log(`App is listening on port ${port}`)
+
+
