9871dd6c7ea2b377fe2829db8050bf136c947b71
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-03-13 01:04:00 +0200

    Fix CI

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
+
 const app = express()
 
 // the __dirname is the current directory from where the script is running
-app.use(cors())
+
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
