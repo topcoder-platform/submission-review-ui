aaee896c92ee4ebb0170ec9d8d2e16ac4c5dffd0
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-03-16 09:21:07 +0200

    Fix account app connector var

--- config/constants/production.js
+++ config/constants/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ACCOUNTS_APP_CONNECTOR_URL: 'https://accounts.topcoder.com/connector.html',
+  ACCOUNTS_APP_CONNECTOR_URL: process.env.ACCOUNTS_APP_CONNECTOR_URL || 'https://accounts.topcoder.com/connector.html',
   ACCOUNTS_APP_LOGIN_URL: 'https://accounts.topcoder.com/member',
   COMMUNITY_APP_URL: 'https://www.topcoder.com',
   MEMBER_API_URL: 'http://api.topcoder.com/v4/members',
