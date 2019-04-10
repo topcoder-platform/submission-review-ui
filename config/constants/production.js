73e396a149cd81645768c585a35c70be24b4e6d7
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-04-10 22:52:23 +0300

    Change accounts app connector URL

--- config/constants/production.js
+++ config/constants/production.js
@@ -1,11 +1,11 @@
 module.exports = {
-  ACCOUNTS_APP_CONNECTOR_URL: process.env.ACCOUNTS_APP_CONNECTOR_URL || 'https://accounts.topcoder-dev.com/connector.html',
+  ACCOUNTS_APP_CONNECTOR_URL: process.env.ACCOUNTS_APP_CONNECTOR_URL || 'https://accounts.topcoder.com/connector.html',
   ACCOUNTS_APP_LOGIN_URL: 'https://accounts.topcoder.com/member',
   COMMUNITY_APP_URL: 'https://www.topcoder.com',
   MEMBER_API_URL: 'http://api.topcoder.com/v4/members',
   MEMBER_API_V3_URL: 'http://api.topcoder.com/v3/members',
   ARENA_URL: 'https://arena.topcoder.com',
-  DEV_APP_URL: 'https://submission-review-ui.herokuapp.com',
+  DEV_APP_URL: 'https://submission-review.topcoder.com',
   CHALLENGE_API_URL: 'http://api.topcoder.com/v4',
   SUBMISSION_REVIEW_API_URL: 'https://submission-review-api.herokuapp.com'
 }
