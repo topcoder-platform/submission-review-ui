1cc8523c4c15b8734ca592471e64c6fa3ab98108
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-04-10 22:59:36 +0300

    Enforce HTTPS

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = express()
 
 // the __dirname is the current directory from where the script is running
 
+const requireHTTPS = (req, res, next) => {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url)
+  }
+  next()
+}
+
+app.use(requireHTTPS)
+
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
