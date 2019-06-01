cafa6c132cfd71ae829ec08ec294f74deaf83889
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-06-01 15:45:17 +0300

    Increase timeout as a temp fix

--- src/services/axiosWithAuth.js
+++ src/services/axiosWithAuth.js
@@ -29,7 +29,7 @@ export const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json'
   },
-  timeout: 20000
+  timeout: 60000
 })
 
 // request interceptor to pass auth token
