86eba6f880b0f8406dd49007ce91ec1ebe608607
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-06-11 10:36:16 +0300

    Fix lint

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 
 const requireHTTPS = (req, res, next) => {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
     return res.redirect('https://' + req.get('host') + req.url)
   }
   next()
