ad9e45097900eea666a1fbfd689ca046f06c6b99
Thomas Kranitsas <thomas.kranitsas@topcoder.com>
2023-05-10 19:45:31 +0300

    add missing V5_API_URL in prod

--- config/constants/production.js
+++ config/constants/production.js
@@ -6,6 +6,7 @@ module.exports = {
   ARENA_URL: process.env.ARENA_URL || 'https://arena.topcoder.com',
   DEV_APP_URL: process.env.DEV_APP_URL || 'https://submission-review.topcoder.com',
   SUBMISSION_REVIEW_API_URL: process.env.SUBMISSION_REVIEW_API_URL || 'https://submission-review-api.topcoder.com',
+  V5_API_URL: process.env.V5_API_URL || 'https://api.topcoder.com/v5',
   FILESTACK_API_KEY: process.env.FILESTACK_API_KEY,
   FILESTACK_SUBMISSION_CONTAINER: process.env.FILESTACK_SUBMISSION_CONTAINER,
   FILESTACK_REGION: process.env.FILESTACK_REGION || 'us-east-1'
