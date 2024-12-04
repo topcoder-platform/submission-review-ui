a6ce5d69922355d83351244a5cec4917406de7cf
Justin Gasper <jmgasper@gmail.com>
2024-12-04 14:48:07 +1100

    Just remove open redirect - service won't be accessible over anything other than HTTPS, enforced by cloudfront rules

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const healthCheck = require('topcoder-healthcheck-dropin')
 
 const app = express()
 
-const requireHTTPS = (req, res, next) => {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
-    return res.redirect('https://' + req.get('host') + req.url)
-  }
-  next()
-}
-
 function check () {
   // Return 200 OK for health check
   return true
